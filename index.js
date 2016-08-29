f1fc96696be0d26ed63f4a3d14ce2b593fc175c7
Robert Vogt <robert.vogt@namics.com>
2016-08-29 14:49:09 +0200

    Wortmarke

--- index.js
+++ index.js
@@ -170,228 +170,30 @@ events.pushedButtons = {};
 },{}],6:[function(require,module,exports){
 'use strict';
 
-var _game = require('./game');
-
-var _game2 = _interopRequireDefault(_game);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 var anypixel = require('anypixel');
 var ctx = anypixel.canvas.getContext2D();
+ctx.font = 'bold 18px Arial';
 
+var text = 'Digital. High Five.';
+var textLength = ctx.measureText(text).width + 6;
+var brand = 'Namics.';
+var brandLength = ctx.measureText(brand).width;
 
-var game = new _game2.default(ctx);
+var textX = ctx.canvas.width;
+var textY = ctx.canvas.height / 2 + 6;
 
-document.addEventListener('onButtonDown', function (ev) {});
-document.addEventListener('onButtonUp', function (ev) {});
+setInterval(function () {
+  ctx.fillStyle = '#D21E1E';
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillStyle = '#000000';
+  ctx.fillText(text, textX--, textY);
+  ctx.fillStyle = '#FFFFFF';
+  ctx.fillText(brand, textLength + textX, textY);
 
-},{"./game":8,"anypixel":1}],7:[function(require,module,exports){
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var colors = ['#0099CC', '#BADA55', '#AA0000'];
-
-var DIRECTION = {
-  UP: -1,
-  DOWN: 1
-};
-
-var Fish = function () {
-  function Fish(ctx) {
-    _classCallCheck(this, Fish);
-
-    this.ctx = ctx;
-
-    this.id = Math.floor(Math.random() * 256) + 1;
-
-    this.active = true;
-    this.age = Math.floor(Math.random() * 128);
-    this.color = colors[Math.floor(Math.random() * colors.length)];
-
-    this.width = 4;
-    this.height = 2;
-
-    this.direction = Math.random() < 0.5 ? DIRECTION.UP : DIRECTION.DOWN;
-
-    this.coords = {
-      x: Math.floor(Math.random() * (ctx.canvas.width - this.width)),
-      y: Math.floor(Math.random() * (ctx.canvas.height - this.height))
-    };
-
-    this.velocity = {
-      x: 2,
-      y: 1
-    };
+  if (textX < (textLength + brandLength) * -1) {
+    textX = ctx.canvas.width;
   }
+  // console.info(textX, textLength);
+}, 1000 / 30);
 
-  _createClass(Fish, [{
-    key: 'isActive',
-    value: function isActive() {
-      return this.active;
-    }
-  }, {
-    key: 'isInBounds',
-    value: function isInBounds() {
-      var _coords = this.coords;
-      var x = _coords.x;
-      var y = _coords.y;
-      var _ctx$canvas = this.ctx.canvas;
-      var width = _ctx$canvas.width;
-      var height = _ctx$canvas.height;
-
-      return x >= 0 && x <= width - this.width && y >= 0 && y <= height - this.height;
-    }
-  }, {
-    key: 'hitsVerticalBorder',
-    value: function hitsVerticalBorder() {
-      var _coords2 = this.coords;
-      var x = _coords2.x;
-      var y = _coords2.y;
-      var _ctx$canvas2 = this.ctx.canvas;
-      var width = _ctx$canvas2.width;
-      var height = _ctx$canvas2.height;
-
-      return x <= 0 || x >= width - this.width;
-    }
-  }, {
-    key: 'hitsHorizontalBorder',
-    value: function hitsHorizontalBorder() {
-      var _coords3 = this.coords;
-      var x = _coords3.x;
-      var y = _coords3.y;
-      var _ctx$canvas3 = this.ctx.canvas;
-      var width = _ctx$canvas3.width;
-      var height = _ctx$canvas3.height;
-
-      return y <= 0 || y >= height - this.height;
-    }
-  }, {
-    key: 'draw',
-    value: function draw() {
-      this.ctx.fillStyle = this.color;
-      this.ctx.fillRect(this.coords.x, this.coords.y, this.width, this.height);
-    }
-  }, {
-    key: 'update',
-    value: function update() {
-      this.coords.x = this.coords.x + this.velocity.x;
-      this.age++;
-
-      if (this.hitsVerticalBorder()) {
-        this.velocity.x = this.velocity.x * -1;
-      }
-
-      this.velocity.y = 0.25 * Math.sin(this.age * Math.PI / 64);
-
-      if (this.hitsHorizontalBorder()) {
-        this.coords.y = this.coords.y < 1 ? this.height : this.ctx.canvas.height - this.height * 2;
-
-        if (this.coords.y > 1 && this.velocity.y < 0) {
-          this.velocity.y = this.velocity.y * -1;
-          console.info(this.coords.y, this.velocity.y);
-        }
-      } else {
-        this.coords.y = this.coords.y + this.velocity.y;
-      }
-      // if (!this.isInBounds()) {
-      //   console.info('Not in bound');
-      //
-      //
-      //   // this.coords.x = this.coords.x + this.velocity.x*3;
-      //   // this.coords.y = this.coords.y + this.velocity.y*3;
-      // }
-
-      // this.active = this.active && this.isInBounds();
-    }
-  }]);
-
-  return Fish;
-}();
-
-exports.default = Fish;
-
-},{}],8:[function(require,module,exports){
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _fish = require('./fish');
-
-var _fish2 = _interopRequireDefault(_fish);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var FPS = 15;
-var MAX_FISHES = 25;
-var FONT_STYLE = '18px Arial';
-
-var Game = function () {
-  function Game(ctx) {
-    _classCallCheck(this, Game);
-
-    this.ctx = ctx;
-    this.ctx.font = FONT_STYLE;
-
-    this.fishes = [];
-
-    for (var i = 0; i < MAX_FISHES; i++) {
-      this.fishes.push(new _fish2.default(this.ctx));
-    }
-
-    this.startLoop();
-  }
-
-  _createClass(Game, [{
-    key: 'startLoop',
-    value: function startLoop() {
-      var _this = this;
-
-      this.interval = setInterval(function () {
-        _this.render();
-      }, 1000 / FPS);
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      var _ctx$canvas = this.ctx.canvas;
-      var width = _ctx$canvas.width;
-      var height = _ctx$canvas.height;
-
-      this.ctx.clearRect(0, 0, width, height);
-
-      this.fishes.forEach(function (fish) {
-        return fish.update();
-      });
-      this.fishes = this.fishes.filter(function (fish) {
-        return fish.isActive();
-      });
-
-      if (this.fishes.length < MAX_FISHES) {
-        this.fishes.push(new _fish2.default(this.ctx));
-      }
-
-      this.fishes.forEach(function (fish) {
-        return fish.draw();
-      });
-    }
-  }]);
-
-  return Game;
-}();
-
-exports.default = Game;
-
-},{"./fish":7}]},{},[6]);
+},{"anypixel":1}]},{},[6]);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const anypixel = require('anypixel');
 const ctx = anypixel.canvas.getContext2D();
-import Game from './game';
+ctx.font = 'bold 18px Arial';
 
-const game = new Game(ctx);
+const text = 'Digital. High Five.';
+const textLength = ctx.measureText(text).width + 6;
+const brand = 'Namics.';
+const brandLength = ctx.measureText(brand).width;
 
-document.addEventListener('onButtonDown', (ev) => {});
-document.addEventListener('onButtonUp', (ev) => {});
+var textX = ctx.canvas.width;
+var textY = ctx.canvas.height / 2 + 6;
+
+setInterval(() => {
+  ctx.fillStyle = '#D21E1E';
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillStyle = '#000000';
+  ctx.fillText(text, textX--, textY);
+  ctx.fillStyle = '#FFFFFF';
+  ctx.fillText(brand, textLength + textX, textY);
+
+  if (textX < ((textLength + brandLength) * -1)) {
+    textX = ctx.canvas.width;
+  }
+  // console.info(textX, textLength);
+}, 1000/30);
